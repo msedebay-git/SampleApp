b8715cc3982d911ec51b34aabd1334b53b1f4817
Talent Network <mohammedsedebay@gmail.com>
2021-02-11 20:07:21 -0500

    updated Hello.java and added a new file logout.java

---
+++ Logout.java
@@ -0,0 +1,7 @@
+// This code will print a statement on commandline
+public class RegexMatch {
+     public static void main(string args[]){
+          system.out.println("Hello, World");
+     }
+}
+
